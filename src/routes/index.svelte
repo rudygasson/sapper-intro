b489ef9dd8feb65c1b504a51cf123f776c5b0e3b
Rudy Gasson <2715405+rudygasson@users.noreply.github.com>
2020-09-24 17:29:44 +0200

    Finish client side fetching

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,136 @@
+<script>
+    import { createEventDispatcher, onMount, onDestroy } from "svelte";
+    import { scale } from "svelte/transition";
+    import { flip } from "svelte/animate";
+    import meetups from "../meetups-store";
+    import MeetupItem from "../components/Meetup/MeetupItem.svelte";
+    import MeetupFilter from "../components/Meetup/MeetupFilter.svelte";
+    import Button from "../components/UI/Button.svelte";
+    import EditMeetup from "../components/Meetup/EditMeetup.svelte";
+    import LoadingSpinner from "../components/UI/LoadingSpinner.svelte";
+
+    let fetchedmeetups = [];
+
+    let editMode = null;
+    let editedId;
+    let isLoading = true;
+
+    const dispatch = createEventDispatcher();
+    let isFiltered = false;
+    let error;
+    let unsubscribe;
+
+    $: filteredMeetups = isFiltered
+        ? fetchedmeetups.filter((m) => m.isFavorite)
+        : fetchedmeetups;
+
+    onMount(() => {
+        isLoading = true;
+        unsubscribe = meetups.subscribe((items) => (fetchedmeetups = items));
+        fetch("https://svelte-course-6dd11.firebaseio.com/meetups.json")
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("An error occurred, please try again!");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                const loadedMeetups = Object.keys(data).map((key) => {
+                    return { id: key, ...data[key] };
+                });
+                isLoading = false;
+                meetups.setMeetups(loadedMeetups.reverse());
+            })
+
+            .catch((err) => {
+                error = err;
+                isLoading = false;
+                console.log(err);
+            });
+    });
+
+    onDestroy(() => {
+        if (unsubscribe) {
+            unsubscribe();
+        }
+    });
+
+    function setFilter(event) {
+        isFiltered = event.detail;
+    }
+
+    function savedMeetup(event) {
+        editMode = null;
+        editedId = null;
+    }
+
+    function cancelEdit(event) {
+        editMode = null;
+        editedId = null;
+    }
+
+    function startEdit(event) {
+        editMode = "edit";
+        editedId = event.detail;
+    }
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+    #meetups {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 1rem;
+    }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+    #meetup-controls {
+        margin: 1rem;
+        display: flex;
+        justify-content: space-between;
+    }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    @media (min-width: 768px) {
+        #meetups {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    #no-meetups {
+        margin: 1rem;
+    }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+    <title>All meetups</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+{#if editMode === 'edit'}
+    <EditMeetup id={editedId} on:save={savedMeetup} on:cancel={cancelEdit} />
+{/if}
+{#if isLoading}
+    <LoadingSpinner />
+{:else}
+    <section id="meetup-controls">
+        <MeetupFilter {isFiltered} on:select={setFilter} />
+        <Button on:click={() => dispatch('edit', null)}>New Meetup</Button>
+    </section>
+    {#if filteredMeetups.length === 0}
+        <p id="no-meetups">No meetups found, you can start adding some.</p>
+    {/if}
+    <section in:scale id="meetups">
+        {#each filteredMeetups as meetup (meetup.id)}
+            <div transition:scale|local animate:flip={{ duration: 600 }}>
+                <MeetupItem
+                    id={meetup.id}
+                    title={meetup.title}
+                    subtitle={meetup.subtitle}
+                    address={meetup.address}
+                    description={meetup.description}
+                    imageUrl={meetup.imageUrl}
+                    isFavorite={meetup.isFavorite}
+                    on:showdetails
+                    on:edit />
+            </div>
+        {/each}
+    </section>
+{/if}
